c5b718549985d18732764700017c62e5316b1bf9
WaWa <142501996+wawa2009@users.noreply.github.com>
2026-04-10 19:43:20 +0700

    Fix case sensitivity in script URL

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wawa2009/Forsaken-script/refs/heads/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wawa2009/Forsaken-script/refs/heads/main/Main.lua"))()
